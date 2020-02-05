29acb38273b8efd4257c9920bca3d7460017ba38
SanteriSuomi <sant.suomi@gmail.com>
2020-02-05 23:33:56 +0200

    add fbm simplex noise function

--- Voxel Worlds/Assets/Scripts/Noise/NoiseUtils.cs
+++ Voxel Worlds/Assets/Scripts/Noise/NoiseUtils.cs
@@ -1,73 +1,46 @@
-﻿using UnityEngine;
-
-namespace Voxel.Noise
+﻿namespace Voxel.Noise
 {
-    public static class NoiseUtils
+    public class NoiseUtils
     {
-        static NoiseUtils()
+        public static NoiseUtils Instance { get; set; }
+
+        private readonly FastNoise noiseGenerator;
+
+        public NoiseUtils()
         {
-            xNoise.Seed = 098123567;
+            noiseGenerator = new FastNoise();
+            noiseGenerator.SetNoiseType(FastNoise.NoiseType.Simplex);
+            noiseGenerator.SetSeed(12309856);
+            noiseGenerator.SetFrequency(0.01f);
+            if (Instance == null)
+            {
+                Instance = this;
+            }
         }
 
-        public static float Noise2D(int x, int y)
-        {
-            return xNoise.CalcPixel2D(x, y, 1);
-        }
-
-        public static float Noise3D(int x, int y, int z)
-        {
-            return xNoise.CalcPixel3D(x, y, z, 1);
-        }
-
-        //private static int fBmOctaves = 1; // Number of noise "loops"
-        //private static float fBmLacunarity = 2; // Steps
-        //private static float fBmGain = 0.5f; // Decrease/increase of amplitude
-        //private static float fBmAmplitude = 1; // How tall features are
-        //private static float fBmFrequency = 1; // Number of feature points
-
-        //public static void SetfBmProperties(int octaves, float lacunarity, float gain, float amplitude, float frequency)
-        //{
-        //    fBmOctaves = octaves;
-        //    fBmLacunarity = lacunarity;
-        //    fBmGain = gain;
-        //    fBmAmplitude = amplitude;
-        //    fBmFrequency = frequency;
-        //}
-
-        //public static float fBm1D(float x)
-        //{
-        //    float value = 0;
-        //    float amplitude = fBmAmplitude;
-        //    float frequency = fBmFrequency;
-        //    for (int i = 0; i < fBmOctaves; i++)
-        //    {
-        //        value += amplitude * Noise2D((int)x, 1);
-        //        amplitude *= fBmGain;
-        //        frequency *= fBmLacunarity;
-        //    }
-            
-        //    return value;
-        //}
-
-        public static float fBm2D(float x, float y)
+        public float fBm2D(float x, float y)
         {
             int octaves = 2;
             float frequency = 1;
             float amplitude = 1;
-            float gain = 0.5f;
             float lacunarity = 2;
+            float gain = 0.5f;
+            float scale = 0.005f;
 
-            float value = 0;
+            float finalValue = 0;
             for (int i = 0; i < octaves; i++)
             {
-                //value += Mathf.PerlinNoise(x * frequency, y * frequency) * amplitude;
-                //xNoise.Seed = Random.Range(0, 1000);
-                value += Noise2D((int)(x * frequency), (int)(y * frequency)) * amplitude;
+                finalValue += Absolute(noiseGenerator.GetNoise(x * frequency, y * frequency, 1)) * amplitude;
                 frequency *= lacunarity;
                 amplitude *= gain;
             }
 
-            return value * 0.00001f; // This OpenSimpleX implementation returns 0-255 so we need to make it in range of 0-255
+            float Absolute(float value)
+            {
+                return (value >= 0) ? value : -value;
+            }
+
+            return finalValue * scale;
         }
     }
 }
--- Voxel Worlds/Assets/Scripts/Noise/noiseutilstest.cs
+++ Voxel Worlds/Assets/Scripts/Noise/noiseutilstest.cs
@@ -4,19 +4,19 @@ using Voxel.Noise;
 public class noiseutilstest : MonoBehaviour
 {
     Terrain terrain;
+    NoiseUtils noise;
 
     private void Awake()
     {
+        noise = new NoiseUtils();
+
         terrain = GetComponent<Terrain>();
         float[,] terrainheightmap = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapResolution, terrain.terrainData.heightmapResolution);
         for (int i = 0; i < terrainheightmap.GetLength(0); i++)
         {
             for (int j = 0; j < terrainheightmap.GetLength(1); j++)
             {
-                //float xPos = i / (float)terrain.terrainData.heightmapResolution;
-                //float yPos = j / (float)terrain.terrainData.heightmapResolution;
-                terrainheightmap[i, j] = NoiseUtils.fBm2D(i, j);
-                //Debug.Log(terrainheightmap[i, j]);
+                terrainheightmap[i, j] = NoiseUtils.Instance.fBm2D(i, j);
             }
         }
 
